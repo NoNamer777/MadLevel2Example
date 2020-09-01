2a03fbb83d2c2919922b24b96b23df44eca642ab
Oscar Wellner <o.wellner@telfort.nl>
2020-09-01 12:18:22 +0200

    Step 2: Enabled view binding, added data class for Reminder and its adapter.

---
+++ app/src/main/java/com/nonamer777/madlevevl2example/ReminderAdapter.kt
@@ -0,0 +1,36 @@
+package com.nonamer777.madlevevl2example
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nonamer777.madlevevl2example.databinding.ItemReminderBinding
+import com.nonamer777.madlevevl2example.model.Reminder
+
+class ReminderAdapter(private val reminders: List<Reminder>):
+    RecyclerView.Adapter<ReminderAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        val binding = ItemReminderBinding.bind(itemView)
+
+        fun dataBind(reminder: Reminder) {
+            binding.tvReminder.text = reminder.reminderText
+        }
+    }
+
+    /**
+     * Creates and returns a ViewHolder object,
+     * inflating a standard layout called `simple_list_item_1`.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_reminder, parent, false)
+    )
+
+    /** Returns the size of the list. */
+    override fun getItemCount(): Int = reminders.size
+
+    /** Called by RecyclerView to display the data at the specified position. */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.dataBind(reminders[position])
+}
---
+++ app/src/main/java/com/nonamer777/madlevevl2example/model/Reminder.kt
@@ -0,0 +1,5 @@
+package com.nonamer777.madlevevl2example.model
+
+data class Reminder (
+    var reminderText: String
+)
